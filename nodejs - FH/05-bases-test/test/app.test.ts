6378da99b5dc198d41d6c50edd62cb317867b273
Jaime Silva Lorca <jsilval@minpublico.cl>
2024-07-12 17:49:40 -0400

    test

---
+++ nodejs - FH/05-bases-test/test/app.test.ts
@@ -0,0 +1,12 @@
+describe('App',()=>{
+    test('Test in the App File',()=>{
+
+
+        const num1 = 10;
+        const num2 = 20;
+
+        const result = num1 + num2
+
+        expect(result).toBe(30)
+    })
+})
